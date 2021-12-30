c89061dd90cb2b1f2ea215e24057255bea7b5380
Simon Kapell <simon@hype1000.de>
2021-12-30 13:23:30 +0100

    add jet-token verification

--- index.js
+++ index.js
@@ -12,31 +12,41 @@ app.use(express.urlencoded({extended: true}));
 // lifecycle API implementation
 app.post('/lifecycle/install', (req, res) => {
     console.log('Received life cycle event: uninstall %s', req.body.token);
-    let decodedToken = jwt.decode(req.body.token);
-    console.log('decoded header: %j', decodedToken.header);
-    console.log('decoded payload: %j', decodedToken.payload);
-    if (decodedToken.payload.iss.indexOf('coyo') >= 0) {
-        console.log('Successful installation');
-        res.status(201).json({code: 100, message: 'ok'});
-    } else {
-        console.log('Unsupported COYO instance');
-        res.status(400).json({code: 101, message: 'Unsupported COYO instance'})
-    }
+    jwt.verify(req.body.token, (err, result) => {
+        if (result) {
+            console.log('verified payload: %j', result.payload);
+            if (result.payload.iss.indexOf('coyo') >= 0) {
+                console.log('Successful installation');
+                res.status(201).json({code: 100, message: 'ok'});
+            } else {
+                console.log('Unsupported COYO instance');
+                res.status(400).json({code: 101, message: 'Unsupported COYO instance'})
+            }
+        } else {
+            console.log('Error validating JWT: %s', err.message);
+            res.status(400).json({code: 401, message: 'Token signature invalid'})
+        }
+    });
 });
 
 // lifecycle API implementation
 app.post('/lifecycle/uninstall', (req, res) => {
     console.log('Received life cycle event: uninstall %s', req.body.token);
-    let decodedToken = jwt.decode(req.body.token);
-    console.log('decoded header: %j', decodedToken.header);
-    console.log('decoded payload: %j', decodedToken.payload);
-    if (decodedToken.payload.iss.indexOf('coyo') >= 0) {
-        console.log('Successful uninstallation');
-        res.status(201).json({code: 100, message: 'ok'});
-    } else {
-        console.log('Unsupported COYO instance');
-        res.status(400).json({code: 101, message: 'Unsupported COYO instance'})
-    }
+    jwt.verify(req.body.token, (err, result) => {
+        if (result) {
+            console.log('verified payload: %j', result.payload);
+            if (result.payload.iss.indexOf('coyo') >= 0) {
+                console.log('Successful uninstallation');
+                res.status(201).json({code: 100, message: 'ok'});
+            } else {
+                console.log('Unsupported COYO instance');
+                res.status(400).json({code: 101, message: 'Unsupported COYO instance'})
+            }
+        } else {
+            console.log('Error validating JWT: %s', err.message);
+            res.status(400).json({code: 401, message: 'Token signature invalid'})
+        }
+    });
 });
 
 // start the server listening for requests
--- jwt/index.js
+++ jwt/index.js
@@ -1,8 +1,38 @@
 const jwt = require('jsonwebtoken');
+const jwkToPem = require('jwk-to-pem');
+
+function getKey(header, callback){
+    if (!header.jku || header.jku.indexOf('https://certificates.plugins.coyoapp.com/') < 0) {
+        callback(new Error('Unknown or untrusted certificate URL: ' + header.jku), null);
+        return;
+    }
+
+    https.get(header.jku, (resp) => {
+        let data = '';
+
+        resp.on('data', (chunk) => {
+            data += chunk;
+        });
+
+        resp.on('end', () => {
+            const key = JSON.parse(data);
+            const pem = jwkToPem(key.key);
+            callback(null, pem);
+        });
+
+    }).on("error", (err) => {
+        callback(err, null);
+    });
+}
+
 
 module.exports = {
     decode: (token) => {
-        let decodedToken = jwt.decode(token, {complete: true});
-        return decodedToken;
+        return jwt.decode(token, {complete: true});
+    },
+    verify: (token, callback) => {
+        jwt.verify(token, getKey, {complete: true}, function(err, decoded) {
+            callback(err, decoded);
+        });
     }
 }
