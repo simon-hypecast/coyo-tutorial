501c5a7442417480e49ba1a1e1c5cb7a68ed5fcb
Simon Kapell <simon@hype1000.de>
2021-12-30 10:29:59 +0100

    bugfix

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const path = require('path');
 const PORT = process.env.PORT || 5001;
 
 // use the express-static middleware
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'dist')));
 
 // start the server listening for requests
 app.listen(PORT,() => console.log(`Server is running on ${ PORT }`));
